f7f07676826c11409fc44ee62747bdd5fe167b54
suprithabheemreddy <69784967+suprithabheemreddy@users.noreply.github.com>
2022-03-31 21:12:00 +0530

    Create positive numbers.py

---
+++ positive numbers.py
@@ -0,0 +1,11 @@
+program to print positive Numbers in given range
+  
+start, end = 12, -7
+  
+# iterating each number in list
+for num in range(start, end + 1):
+      
+    # checking condition
+    if num >= 0:
+        print(num, end = " ")
+Output:
